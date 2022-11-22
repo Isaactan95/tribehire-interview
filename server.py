a197474d66009df9773832a1aaf50046318b752d
Isaac <isaac_ann@hotmail.com>
2022-11-22 22:34:49 +0800

    test

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+isConnected = False
+
+@app.before_first_request
+def get_data():
+    pass
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run()
